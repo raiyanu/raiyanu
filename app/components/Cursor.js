9290612fdf0ce09c7ecf5380d4030861365fd34f
Raiyan Ahmed <ray38250968@gmail.com>
2024-11-01 19:31:42 +0000

    chore: cursor style fix and content affect

--- app/components/Cursor.js
+++ app/components/Cursor.js
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 export default function Cursor({ children, hideBlob }) {
     const section = useRef(null);
-    const [blobSize, setBlobSize] = useState(30);
+    const [blobSize, setBlobSize] = useState(50);
     const { x, y } = useMousePosition({ section });
     const handleMaskEnter = () => {
         setBlobSize(50);
@@ -25,17 +25,19 @@ export default function Cursor({ children, hideBlob }) {
                 ref={section}
                 className={`fixed rounded-full pointer-events-none bg-secondary-content bg-opacity-30 border-primary-content z-[1] ${hideBlob ? "border-none" : "border-2"
                     }`}
-                style={{ left: x - blobSize / 2, top: y - blobSize / 2 }}
                 animate={{
                     height: hideBlob ? 0 : blobSize,
                     width: hideBlob ? 0 : blobSize,
                     transition: {
-                        duration: 0.5,
+                        duration: 0.24,
                         times: [0, 0.5, 1],
+                        top: { duration: 0.15 },
+                        left: { duration: 0.15 },
                     },
+                    left: x - blobSize / 2,
+                    top: y - blobSize / 2,
                 }}
-            >
-            </motion.div>
+            ></motion.div>
 
             {children}
         </div>
--- app/section/Hero.js
+++ app/section/Hero.js
@@ -12,12 +12,23 @@ export default function Hero({ blob }) {
             <div className="h-fit w-full flex flex-col">
                 <div className="min-h-screen flex items-center justify-center flex-col">
                     <div className="max-w-2xl w-full min-h-[80vh] flex flex-col justify-center pt-12">
-                        <h1 className="text-4xl font-bold mb-4 text-gray-800 max-w-lg ">
+                        <h1 className="text-4xl font-bold mb-4 text-gray-800 max-w-lg"
+
+                        >
                             Hello,
                             I{"'"}m a full stack developer
                         </h1>
                         <h2 className="text-2xl font-semibold mb-4 text-gray-800">
-                            My name{"'"}s <span className="text-4xl ">Raiyan Ahmed.</span> <br />I build web applications using modern web
+                            My name{"'"}s <motion.span className="text-4xl cursor-pointer text-secondary-content rounded-md" animate={{
+                                backgroundColor: [!blob.hideBlob ? "transparent" : "#000"],
+                            }}
+                                onMouseEnter={() => {
+                                    blob.setHideBlob(true)
+                                }}
+                                onMouseLeave={() => {
+                                    blob.setHideBlob(false)
+                                }}
+                            >Raiyan Ahmed.</motion.span> <br />I build web applications using modern web
                             technologies.
                         </h2>
                         <button className="px-8 py-3 bg-primary-bg rounded-full font-medium hover:bg-secondary-bg transition duration-300 block my-4 w-fit"
